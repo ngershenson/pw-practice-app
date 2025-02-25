eeb446db4f6a65ec84e9cadb13baf9db9c6bb420
ngershenson <ndgershe@gmail.com>
2025-02-25 14:35:26 -0500

    using parent element locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,51 +1,105 @@
-import { test } from '@playwright/test';
+import { test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:4200/'); // , { timeout: 120000, waitUntil: 'domcontentloaded'}
-    await page.getByText('Forms').click();
-    await page.getByText('Form Layouts').click();
+  await page.goto("http://localhost:4200/"); // , { timeout: 120000, waitUntil: 'domcontentloaded'}
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
 });
 
-test('Locator syntax rules', async({ page }) => {
-    // by Tag name
-    await page.locator('input').first().click()
+test("Locator syntax rules", async ({ page }) => {
+  // by Tag name
+  await page.locator("input").first().click();
 
-    // by ID
-    page.locator('#inputEmail1')
+  // by ID
+  page.locator("#inputEmail1");
 
-    // by class value
-    page.locator('.shape-rectangle')
+  // by class value
+  page.locator(".shape-rectangle");
 
-    // by attribute
-    page.locator('[type="email"]')
+  // by attribute
+  page.locator('[type="email"]');
 
-    // by Class value (full)
-    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+  // by Class value (full)
+  page.locator(
+    '[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]'
+  );
 
-    // combine different selectors
-    page.locator('input[placeholder="Email"]')
+  // combine different selectors
+  page.locator('input[placeholder="Email"]');
 
-    // by XPath (NOT RECOMMENDED)
-    page.locator('//*[@id="inputEmail1"]')
+  // by XPath (NOT RECOMMENDED)
+  page.locator('//*[@id="inputEmail1"]');
 
-    // by partial text match
-    page.locator(':text("Using")')
+  // by partial text match
+  page.locator(':text("Using")');
 
-    // by exact text match
-    page.locator(':text-is("Using the Grid")')
-})
+  // by exact text match
+  page.locator(':text-is("Using the Grid")');
+});
 
-test('User facing locators', async({ page }) => {
-    await page.getByRole('textbox', {name: "Email"}).first().click()
-    await page.getByRole('button', {name: "Sign in"}).first().click()
+test("User facing locators", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click();
+  await page.getByRole("button", { name: "Sign in" }).first().click();
 
-    await page.getByLabel('Email').first().click()
+  await page.getByLabel("Email").first().click();
 
-    await page.getByPlaceholder('Jane Doe').click()
+  await page.getByPlaceholder("Jane Doe").click();
 
-    await page.getByText('Using the Grid').click()
+  await page.getByText("Using the Grid").click();
 
-    await page.getByTestId('SignIn').click()
+  await page.getByTestId("SignIn").click();
 
-    await page.getByTitle('Iot Dashboard').click()
+  await page.getByTitle("Iot Dashboard").click();
+});
+
+test("locating child elements", async ({ page }) => {
+  await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+  await page
+    .locator("nb-card")
+    .locator("nb-radio")
+    .locator(':text-is("Option 2")')
+    .click();
+
+  await page
+    .locator("nb-card")
+    .getByRole("button", { name: "Sign in" })
+    .first()
+    .click();
+
+  await page.locator("nb-card").nth(3).getByRole("button").click();
+});
+
+test("locating parent elements", async ({ page }) => {
+  await page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  await page
+    .locator("nb-card", { has: page.locator("#inputEmail1") })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic form" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator(".status-danger") })
+    .getByRole("textbox", { name: "Password" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator("nb-checkbox") })
+    .filter({ hasText: "Sign in" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator(':text-is("Using the Grid")')
+    .locator('..')
+    .getByRole("textbox", { name: "Email" })
+    .click();
 });
