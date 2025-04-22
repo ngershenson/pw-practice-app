1b187337e6a1c0b6a5c5994a865fd5a4827bd239
Noah Gershenson <ndgershe@gmail.com>
2025-04-22 10:02:23 -0400

    form layouts page object

---
+++ page-objects/formLayoutsPage.ts
@@ -0,0 +1,47 @@
+import { Page } from "@playwright/test";
+
+export class FormLayoutsPage {
+  private readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async submitUsingTheGridFormWithCredentialsAndSelectOption(
+    email: string,
+    password: string,
+    optionText: string
+  ) {
+    const usingTheGridForm = this.page.locator("nb-card", {
+      hasText: "Using the Grid",
+    });
+    await usingTheGridForm.getByRole("textbox", { name: "Email" }).fill(email);
+    await usingTheGridForm.getByLabel("Password").fill(password);
+    await usingTheGridForm
+      .getByRole("radio", { name: optionText })
+      .check({ force: true });
+    await usingTheGridForm.getByRole("button").click();
+  }
+
+  /**
+   * Submit the inline form with name, email, and remember me option.
+   * @param name - should be first and last name
+   * @param email - should be email
+   * @param rememberMe - should be true or false
+   */
+  async submitInlineFormWithNameEmailAndCheckbox(
+    name: string,
+    email: string,
+    rememberMe: boolean
+  ) {
+    const inlineForm = this.page.locator("nb-card", { hasText: "Inline form" });
+    await inlineForm.getByPlaceholder("Jane Doe").fill(name);
+    await inlineForm.getByPlaceholder("Email").fill(email);
+    if (rememberMe) {
+      await inlineForm.getByLabel("Remember me").check({ force: true });
+    } else {
+      await inlineForm.getByLabel("Remember me").uncheck({ force: true });
+    }
+    await inlineForm.getByRole("button").click();
+  }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,15 +1,33 @@
 import { test, expect } from "@playwright/test";
 import { NavigationPage } from "../page-objects/navigationPage";
+import { FormLayoutsPage } from "../page-objects/formLayoutsPage";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
 });
 
 test("navigate to form page", async ({ page }) => {
-    const navigateTo = new NavigationPage(page);
-    await navigateTo.formLayoutsPage();
-    await navigateTo.datepickerPage();
-    await navigateTo.smartTablePage();
-    await navigateTo.toastrPage();
-    await navigateTo.tooltipPage();
+  const navigateTo = new NavigationPage(page);
+  await navigateTo.formLayoutsPage();
+  await navigateTo.datepickerPage();
+  await navigateTo.smartTablePage();
+  await navigateTo.toastrPage();
+  await navigateTo.tooltipPage();
+});
+
+test("parametrized methods", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  const onFormLayoutsPage = new FormLayoutsPage(page);
+
+  await navigateTo.formLayoutsPage();
+  await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption(
+    "test@example.com",
+    "password",
+    "Option 1"
+  );
+  await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(
+    "John Smith",
+    "john.test@test.com",
+    true
+  );
 });
